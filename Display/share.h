8d185e51bca8db27870087371f39267eb4053364
t4skforce1337 <t4skforce1337@gmail.com>
2016-12-16 10:09:51 -0800

    * init checkin * IPC

---
+++ Display/share.h
@@ -0,0 +1,17 @@
+/*
+ * share.h
+ *
+ *  Created on: Dec 16, 2016
+ *      Author: bssp
+ */
+
+#ifndef SHARE_H_
+#define SHARE_H_
+
+#define MAX_MSG_LEN 256
+
+#define QUEUE_NAME "/is131415"
+
+#define MAX_MSG_COUNT 10
+
+#endif /* SHARE_H_ */
